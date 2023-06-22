6eca4ac6ccd7846c412b2ac18f9425fa9122bfc5
pedro <fabrica.software.ltda@outlook.com>
2023-06-21 23:44:39 -0300

    adding more controllers

--- ComplaintsAPI/Controllers/ComplaintsController.cs
+++ ComplaintsAPI/Controllers/ComplaintsController.cs
@@ -12,11 +12,30 @@ namespace ComplaintsAPI.Controllers
         private readonly IComplaintsService _complaintsService;
         public ComplaintsController(IComplaintsService complaints) {
             _complaintsService= complaints;
-
         }
         [HttpPost("set")]
-        public ActionResult<string> set([FromBody] ComplaintsModel complaints) {
+        public ActionResult<string> Set([FromBody] ComplaintsModel complaints) {
             return _complaintsService.Set(complaints);
+        }
+        [HttpPost("get")]
+        public ActionResult<List<ComplaintsModel>> Get()
+        {
+            return _complaintsService.Get();
+        }
+        [HttpPost("getwithid")]
+        public ActionResult<ComplaintsModel> Getwithid([FromBody] string TicketID)
+        {
+            return _complaintsService.GetSpecific(TicketID);
+        }
+        [HttpPost("update")]
+        public ActionResult<ComplaintsModel> Update([FromBody] ComplaintsModel complaints)
+        {
+            return _complaintsService.Update(complaints);
+        }
+        [HttpPost("delete")]
+        public ActionResult<string> Delete([FromBody] string TicketID)
+        {
+            return _complaintsService.Delete(TicketID);
         }
         //TODO finish the others controllers...
     }
--- ComplaintsAPI/Models/ComplaintsModel.cs
+++ ComplaintsAPI/Models/ComplaintsModel.cs
@@ -14,15 +14,20 @@ namespace ComplaintsAPI.Model
         public string TicketUsername { get; set; } = string.Empty;
         [RegularExpression("^(Valor1|Valor2|Valor3)$", ErrorMessage = "The value of Type only accepts 'Valor1', 'Valor2' ou 'Valor3'.")]
         [BsonElement]
-        public string Type { get; set;} = string.Empty;
+        public string Channel { get; set; } = string.Empty;
         [BsonElement]
+        [RegularExpression("^(Produto com defeito|Produto não chegou |Valor3)$", ErrorMessage = "The value of Type only accepts 'Valor1', 'Valor2' ou 'Valor3'.")]
+        public Enum Type { get; set; }
+        [BsonElement]
+        [RegularExpression("^(Pendente|Encaminhado|Finalizado)$", ErrorMessage = "The value of Type only accepts 'Valor1', 'Valor2' ou 'Valor3'.")]
         public string Description { get; set; } = string.Empty;
         [Range(1, 3, ErrorMessage = "The value of level must be between 1 and 3.")]
         [BsonElement]
-        public string Level { get; set; } = string.Empty;
+        public Enum Level { get; set; }
         [BsonElement]
+
         public string? Resolution { get; set; }
-        [BsonElement]
-        public bool IsAnswered;
+
+       public List<string> Photos { get; set; }= new List<string>();
     }
 }
